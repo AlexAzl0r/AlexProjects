d5763073d52b8352707489f8964dde157e074467
U-DESKTOP-SDFK855\alex_ <alex_neville@live.co.uk>
2021-03-13 17:42:17 +0000

    Formatting changes

--- CSMCBudgeting/newmember.py
+++ CSMCBudgeting/newmember.py
@@ -20,7 +20,9 @@ class MembershipUpdate:
 
     def format_new_member(self):
         cols = ['memberName','dateFrom','dateLeft','active']
-        values = [self.name, self.date_joined, None if self.date_left is None else dt.datetime.strptime(self.date_left, "%Y-%m-%d"), True if self.is_active is None else self.is_active]
+        values = [self.name, self.date_joined,
+                  None if self.date_left is None else dt.datetime.strptime(self.date_left, "%Y-%m-%d"),
+                  True if self.is_active is None else self.is_active]
         dataframe = pd.DataFrame([values], columns=cols, index=None)
         return dataframe
 
@@ -34,7 +36,10 @@ class MembershipUpdate:
         self.output_to_database(update_new_member)
 
 if __name__ == "__main__":
-    job = MembershipUpdate(connection=None, name=args.name, date_joined=args.datejoined, date_left=None if args.dateleft is None else args.dateleft)
+    job = MembershipUpdate(connection=None,
+                           name=args.name,
+                           date_joined=args.datejoined,
+                           date_left=None if args.dateleft is None else args.dateleft)
     job.run()
 
 
