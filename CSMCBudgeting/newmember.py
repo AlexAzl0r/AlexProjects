8ed5ee665b32193c2d64b3dbb29e6e24e815ed98
U-DESKTOP-SDFK855\alex_ <alex_neville@live.co.uk>
2021-03-13 17:34:05 +0000

    Update file formats

--- CSMCBudgeting/newmember.py
+++ CSMCBudgeting/newmember.py
@@ -10,31 +10,32 @@ parser.add_argument("--datejoined", help="Date joined - format YYYY-MM-DD")
 parser.add_argument("--dateleft", help="Date Left, leave blank if new member", nargs='?',default=None)
 
 args = parser.parse_args()
-print(args)
 class MembershipUpdate:
-    def __init__(self, name, datejoined, connection, dateleft=None):
+    def __init__(self, name, date_joined, connection, date_left=None):
         self.name = name
-        self.datejoined = datejoined
-        self.dateleft = dateleft
-        self.isactive = None if dateleft is None else False
+        self.date_joined = date_joined
+        self.date_left = date_left
+        self.is_active = None if date_left is None else False
         self.connection = connection
 
-    def formatnewmember(self):
+    def format_new_member(self):
         cols = ['memberName','dateFrom','dateLeft','active']
-        vals = [self.name, self.datejoined,None if self.dateleft is None else dt.datetime.strptime(self.dateleft, "%Y-%m-%d"), True if self.isactive is None else self.isactive]
-        emptydf = pd.DataFrame([vals], columns=cols,index=None)
-        return emptydf
+        values = [self.name, self.date_joined, None if self.date_left is None else dt.datetime.strptime(self.date_left, "%Y-%m-%d"), True if self.is_active is None else self.is_active]
+        dataframe = pd.DataFrame([values], columns=cols, index=None)
+        return dataframe
 
-    def commitmember(self, memberdf):
+    @staticmethod
+    def output_to_database(new_member_dataframe): #staticmethod
         conn = sqlite3.connect("csmc.db")
-        memberdf.to_sql(name="MEMBERS",con=conn,if_exists="append",index=False)
+        new_member_dataframe.to_sql(name="MEMBERS", con=conn, if_exists="append", index=False)
+
+    def run(self):
+        update_new_member = self.format_new_member()
+        self.output_to_database(update_new_member)
 
 if __name__ == "__main__":
-    obj = MembershipUpdate(connection=None,name=args.name, datejoined=args.datejoined,dateleft=None if args.dateleft is None else args.dateleft)
-    df = obj.formatnewmember()
-    print(df)
-    out = obj.commitmember(df)
-
+    job = MembershipUpdate(connection=None, name=args.name, date_joined=args.datejoined, date_left=None if args.dateleft is None else args.dateleft)
+    job.run()
 
 
 
